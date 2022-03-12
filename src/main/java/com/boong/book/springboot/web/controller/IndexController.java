cf8c9bd50d0e58152d1f0575f1f07ec59093de0c
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-12 14:40:40 +0900

    modified build.gradle Junit 버전 문제
     * Junit Platform 버전 문제 해결
       * 원인: Junit5 환경에서 Junit4 기반 테스트 작성
       * 해결: Junit4 환경 추가
              -> junit4 testEngine 추가 설정
              -> testRuntimeOnly junit-vintage-engine
       * 관련 내용 build.gradle 에 주석처리

--- src/main/java/com/boong/book/springboot/web/controller/IndexController.java
+++ src/main/java/com/boong/book/springboot/web/controller/IndexController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
