4c52470407057b3c9db817f3086096021b909ffd
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 19:08:57 +0900

    codedeploy 설정 누락 & 누락 스크립트 추가

---
+++ scripts/stop.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PORT} ]
+then
+  echo "> kill -15 ${IDLE_{ID}}"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
