2676a98e3a3535fe26cfdfaeb1da84912bc3bc0b
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 14:40:54 +0900

    배포 자동화 shell... 수정 오류해결중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Travis, CI, S3, CodeDeploy 연동 성공
-# 실제 Jar 배포 및 실행
-
-# 배포 자동화 구성
-
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=boong-springboot2-webservice
 
@@ -12,16 +7,14 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PIT=$(pgrep -fl boong-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl boong-springboot2-webservice | grep jar | awk '{print $1}')
 
-echo "현재 구동중인 애플리케이션 pid: $CURRENT_PIT"
-if[ -z "$CURRENT_PID"]; then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+if [ -z "$CURRENT_PID" ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PIT"
-  kill -15 $CURRENT_PIT
-  sleep 5
-
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
 echo "> 새 애플리케이션 배포"
@@ -35,7 +28,7 @@ echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-  -Dspring.profiles.active=true \
+  -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
 
