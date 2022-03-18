e3196504c5cb845f7dac3e41aef68af240f37192
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 22:41:44 +0900

    bash 문법 오류

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -14,9 +14,7 @@ function find_idle_profile() {
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
 
-  echo "${CURRENT_PROFILE}"
-
-  if [ ${CURRENT_PROFILE} == real1 ]
+  if [ "${CURRENT_PROFILE}" == real1 ]
   then
     IDLE_PROFILE=real2
   else
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -9,6 +9,8 @@ IDLE_PORT=$(find_idle_port)
 echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:"${IDLE_PORT}")
 
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid: $IDLE_PID"
+
 if [ -z "${IDLE_PORT}" ]
 then
   echo "> kill -15 ${IDLE_{ID}}"
