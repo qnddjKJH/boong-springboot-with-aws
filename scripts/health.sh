c9655b2de79892bb42807d7cc48079db03a3344f
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 22:33:34 +0900

    bash 문법 오류

--- scripts/health.sh
+++ scripts/health.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
-$IDLE_PORT=$(find_idle_port)
+IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start"
 echo "> IDLE_PORT: $IDLE_PORT"
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 # 쉬고 있는 profile 찾기: real1 이 사용 중이면 real2 가 쉬고 있으면 반대면 real1 이 쉼
+# bash 는 return value 가 안되니 *제일 마지막줄에 echo 로 해서 결과 출력*후, 클라이언트에서 값을 사용한다
 
 function find_idle_profile() {
   RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-  if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 즉, 40x/50x 에러 모두 포함
+ # 400 보다 크면 즉, 40x/50x 에러 모두 포함
+  if [ ${RESPONSE_CODE} -ge 400 ]
   then
     CURRENT_PROFILE=real2
   else
     CURRENT_PROFILE=$(curl -s http://localhost/profile)
   fi
+
+  echo "${CURRENT_PROFILE}"
 
   if [ ${CURRENT_PROFILE} == real1 ]
   then
