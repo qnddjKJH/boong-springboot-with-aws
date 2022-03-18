1e001a2afe80401a4ba890a8bf842530ce5ab140
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 18:03:51 +0900

    무중단 배포를 위한 설정 수정 및 배포 스크립트 추가

---
+++ scripts/profile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# 쉬고 있는 profile 찾기: real1 이 사용 중이면 real2 가 쉬고 있으면 반대면 real1 이 쉼
+
+function find_idle_profile() {
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  if [ $(RESPONSE_CODE) -ge 400 ]# 400 보다 크면 즉, 40x/50x 에러 모두 포함
+  then
+    CURRENT_PROFILE=real2
+  else CURRENT_PROFILE=$(curl -s http://localhost/profile)
+  fi
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+  fi
+
+  echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 profile 의 port 찾기
+function find_idle_port() {
+     IDLE_PROFILE=$(find_idle_profile)
+
+     if [ ${IDLE_PROFILE} == real1 ]
+     then
+       echo "8081"
+     else
+       echo "8082"
+     fi
+}
