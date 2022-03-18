245e429de41ced4df5bf01a19e2594244c04db72
qnddjkjh.dev <qnddjkjh.dev@gmail.com>
2022-03-18 22:46:51 +0900

    bash 문법 오류

--- scripts/health.sh
+++ scripts/health.sh
@@ -17,7 +17,7 @@ do
   RESPONSE=$(curl -s http://localhost:$IDLE_PORT/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -1)
 
-  if [ ${UP_COUNT} -ge 1 ]
+  if [ "${UP_COUNT}" -ge 1 ]
   then # UP_COUNT >= 1 'real' 문자열 있는지 검증 1보다 크다면
     echo "> Health Check Success"
     switch_proxy
@@ -27,7 +27,7 @@ do
     echo "> Health Check: ${RESPONSE}"
   fi
 
-  if [ ${RETRY_COUNT} -eq 10 ]
+  if [ "${RETRY_COUNT}" -eq 10 ]
   then
     echo "> Health Check failed"
     echo "> Nginx 에 연결하지 않고 배포를 종료합니다."
